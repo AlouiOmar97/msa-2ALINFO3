0ad16c13e230d29cc807e57e4b325563b6a7d05c
AlouiOmar <alouiomar1997@gmail.com>
2026-06-24 09:17:22 +0100

    Keycloak

---
+++ Candidat/src/main/java/com/esprit/tn/candidat/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.esprit.tn.candidat;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws
+            Exception {
+                http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                // Autoriser Swagger et Actuator
+                                .requestMatchers( "/actuator/**").permitAll()
+                // Toutes les autres requêtes nécessitent un token
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
